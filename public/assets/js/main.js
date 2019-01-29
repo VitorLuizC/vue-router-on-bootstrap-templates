191417fff66bf892e57f76070a0031b7ed5d8390
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-01-29 15:50:03 -0200

    :truck: move menu and scripts to public/index

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,8 @@
 ;(function(){
-
 			// Menu settings
 			$('#menuToggle, .menu-close').on('click', function(){
 				$('#menuToggle').toggleClass('active');
 				$('body').toggleClass('body-push-toleft');
 				$('#theMenu').toggleClass('menu-open');
 			});
-
-
 })(jQuery)
